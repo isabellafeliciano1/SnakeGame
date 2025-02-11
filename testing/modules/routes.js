5e7ac9c475b6f6cd0c1ab3729da5cc8d79942f6b
isabellafeliciano1 <isabe25578@live.ytech.edu>
2025-02-11 06:22:36 -0800

    CSS and Formatting

--- testing/modules/routes.js
+++ testing/modules/routes.js
@@ -78,8 +78,8 @@ function postLogin(req, res) {
 }
 
 function isAuthenticated(req, res, next) {
-  if (req.session.user) next() 
-  else res.redirect('/login')   
+  if (req.session.user) next()
+  else res.redirect('/login')
 }
 
 module.exports = {
